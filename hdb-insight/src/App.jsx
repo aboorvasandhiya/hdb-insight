24ec63dd4b69f97172e3efd9136686aef3672b89
Nur A'liyah Binte Mawasi <2402715@sit.singaporetech.edu.sg>
2025-11-05 14:42:12 +0800

    all

--- hdb-insight/src/App.jsx
+++ hdb-insight/src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import DataManagement from "./pages/DataManagement";
-import Insights from "./pages/Insights";
+import UserDashboard from "./pages/UserDashboard";
+import UserDataManagement from "./pages/UserDataManagement";
+import UserInsights from "./pages/UserInsights";
 import UserAccount from "./pages/UserAccount";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminDataManagement from "./pages/AdminDataManagement";
@@ -14,9 +14,9 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/data" element={<DataManagement />} />
-      <Route path="/insights" element={<Insights />} />
+      <Route path="/dashboard" element={<UserDashboard />} />
+      <Route path="/data" element={<UserDataManagement />} />
+      <Route path="/insights" element={<UserInsights />} />
       <Route path="/account" element={<UserAccount />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/admin-data" element={<AdminDataManagement />} />
